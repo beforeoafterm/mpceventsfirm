d38c54d6b3ccd17e61e20aaaa5fb16419a9c9754
neilpette <neil.pete@live.com>
2016-10-12 08:41:52 +0800

    included application files

---
+++ mpceventsfirm/resources/views/welcome.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Event Management and Marketing | MPC Events Firm Inc. | PH"/>
+    <meta name="description" content="MPC Events Firm Inc. is a provider of professional marketing services that has emerged as one of the most reliable and aggressive marketing support companies."/>
+    <meta name="author" content="ENJA Web Studios"/>
+
+    <title>MPC Events Firm | Personal and Corporate Event Management in the Philippines</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Slabo+27px|Abel">
+
+    <!-- Material Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/css/font-awesome.min.css"/>
+
+    <!--Materialize CSS-->
+    <link rel="stylesheet" href="/css/materialize.min.css" media="screen,projection"/>
+    <link rel="stylesheet" href="/css/app.css"/>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#fbec39">
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js" defer></script>
+    <script src="js/materialize.min.js" defer></script>
+    <style>
+        body {
+            background-color: #222;
+            color: #fafafa;
+        }
+
+        img#logo {
+            height: 40vh;
+        }
+
+        .uppercase {
+            text-transform: uppercase;
+        }
+
+        a.social-media-btn {
+            color: #d4c631;
+            font-size: 2em;
+            padding: 0 10px;
+            border-radius: 10%;
+        }
+
+        a:hover.social-media-btn {
+            color: #fbec39;
+            font-size: 2em;
+            padding: 0 10px;
+            border-radius: 10%;
+        }
+    </style>
+</head>
+<body>
+    <div class="valign-wrapper" style="position: absolute; height: 100%; width: 100%;">
+        <div class="center" style="position: relative; width: 100%;">
+            <div class="row">
+                <div class="col s12">
+                    <img id="logo" src="/img/logo-circle.png" alt="MPC Events Firm Inc.">
+                </div>
+            </div>
+            <div class="container">
+                <h5 class="valign yellow-mpc-text hide-on-med-and-up uppercase">This site is under construction</h5>
+                <h3 class="valign yellow-mpc-text hide-on-small-only uppercase">This site is under construction.</h3>
+                <p>We're aiming to give you the best experience as possible, so we're improving this site to achieve just that.</p>
+                <br>
+                <h6 class="valign hide-on-med-up uppercase">Contact us via Social Media</h6>
+                <a class="social-media-btn" href="http://fb.com/mpceventsfirm" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>
+</a>
+                <a class="social-media-btn" href="http://twitter.com/mpceventsfirm" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>
+</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
